b395acc839250226aaf6fad3362487a8c1e19b9f
Zorglind <abnergabriel_lima@outlook.com>
2025-09-23 17:19:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const app = require('./src/app');
 const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.json({ ok: true, name: 'Gestão & Dashboard API' });
+});
+
 app.listen(PORT, () => console.log(`Backend rodando na porta ${PORT}`));
