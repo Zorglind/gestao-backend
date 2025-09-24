b0a46d816a72174e0ae90895484e7b30666e7440
Zorglind <abnergabriel_lima@outlook.com>
2025-09-24 16:35:45 -0300

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const app = require('./src/app');
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`[gestao-backend] listening on port ${PORT}`);
-});
