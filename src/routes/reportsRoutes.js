832b9456f3bd5e0e5da42c2da21672b77f249999
Zorglind <abnergabriel_lima@outlook.com>
2025-09-25 19:32:36 -0300

    Delete src directory

--- src/routes/reportsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/reportsController');
-
-// === Relatórios (como frontend espera) ===
-router.get('/monthly_indicators', ctrl.getMonthlyIndicators);
-router.get('/payment_methods_summary', ctrl.getPaymentSummary);
-
-module.exports = router;
